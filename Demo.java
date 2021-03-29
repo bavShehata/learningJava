875ff116c44754da751b33950e7e6edec153fa93
bavshehata <bavshehata@gmail.com>
2021-03-29 10:23:19 +0200

    conditionals and office reference

--- Demo.java
+++ Demo.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+
 public class Demo {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello world!");
         int x = 5;
@@ -12,13 +13,12 @@ public class Demo {
         System.out.println(greeting);
         System.out.println("This is " + "concatenation");
 
-
         // 5 + 3 // 8
         // 10 / 3 // 3
         // 10 / 3.0 // 3.333
         // 10 % 3 // 1
         x++;
-        System.out.println(x/3.0);
+        System.out.println(x / 3.0);
 
         boolean a = true, b = false;
 
@@ -26,6 +26,23 @@ public class Demo {
         System.out.println("a || b: " + (a || b));
         System.out.println("!(a && b): " + !(a && b));
 
+        System.out.println("Please enter a grade yasta: ");
+        int grade = input.nextInt();
+        if (grade >= 70)
+            System.out.println("A+!!!");
+        else if (grade >= 60)
+            System.out.println("B!!!");
+        else if (grade >= 50)
+            System.out.println("C!!!");
+        else
+            System.out.println("FAIL");
 
+        switch (grade) {
+        case 69:
+            System.out.println("NICE");
+            break;
+        default:
+            System.out.println("No one cares Toby");
+        }
     }
 }
