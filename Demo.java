95cde2c66eb50371147cef6ee073ec3c30cf0e21
bavshehata <bavshehata@gmail.com>
2021-03-29 09:48:35 +0200

    data types

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        int x = 5;
+        System.out.println(x);
+
+        String greeting = "Hello there my man";
+        System.out.println(greeting.charAt(3));
+    }
+}
