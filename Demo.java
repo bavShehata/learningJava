78e6a961caef667aafea11891e5b38e5468e2660
bavshehata <bavshehata@gmail.com>
2021-04-10 20:00:47 +0200

    loops

--- Demo.java
+++ Demo.java
@@ -4,45 +4,16 @@ public class Demo {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Hello world!");
-        int x = 5;
-        System.out.println(x);
-        System.out.println("Enter your name buddy");
-        String name = input.nextLine();
-        String greeting = "Hello there " + name;
-        System.out.println(greeting);
-        System.out.println("This is " + "concatenation");
-
-        // 5 + 3 // 8
-        // 10 / 3 // 3
-        // 10 / 3.0 // 3.333
-        // 10 % 3 // 1
-        x++;
-        System.out.println(x / 3.0);
-
-        boolean a = true, b = false;
-
-        System.out.println("a && b: " + (a && b));
-        System.out.println("a || b: " + (a || b));
-        System.out.println("!(a && b): " + !(a && b));
-
-        System.out.println("Please enter a grade yasta: ");
-        int grade = input.nextInt();
-        if (grade >= 70)
-            System.out.println("A+!!!");
-        else if (grade >= 60)
-            System.out.println("B!!!");
-        else if (grade >= 50)
-            System.out.println("C!!!");
-        else
-            System.out.println("FAIL");
-
-        switch (grade) {
-        case 69:
-            System.out.println("NICE");
-            break;
-        default:
-            System.out.println("No one cares Toby");
-        }
+        // int x = 5;
+        // int y = 3;
+        // x = ++y;
+        // String name = "Bavly";
+        // name = input.nextLine();
+        // System.out.println(name.charAt(0));
+        // System.out.println(x);
+        int i;
+        for (i = 0; i < 5; i++)
+            System.out.println(i);
+        while (i > 0) System.out.println(i--);
     }
 }
