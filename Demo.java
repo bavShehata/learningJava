7dde0fc3072d7ac15c7a9230912782a027cca2f1
bavshehata <bavshehata@gmail.com>
2021-04-11 16:09:29 +0200

    arrays

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Demo {
 
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         // int x = 5;
@@ -11,9 +12,18 @@ public class Demo {
         // name = input.nextLine();
         // System.out.println(name.charAt(0));
         // System.out.println(x);
-        int i;
-        for (i = 0; i < 5; i++)
-            System.out.println(i);
-        while (i > 0) System.out.println(i--);
+        // int i;
+        // for (i = 0; i < 5; i++)
+        // System.out.println(i);
+        // while (i > 0) System.out.println(i--);
+
+        int x;
+        x = input.nextInt();
+        int[] arr = new int[x];
+        System.out.println(arr.length);
+        arr[0] = 3;
+        int[] a = {0,1,2};
+
+
     }
 }
