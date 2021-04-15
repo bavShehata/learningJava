687b2191f9b816f73fe2a5556812539443e0951a
bavshehata <bavshehata@gmail.com>
2021-04-15 12:07:11 +0200

    mutlilevel inheritance

---
+++ JuniorTech.java
@@ -0,0 +1,8 @@
+public class JuniorTech extends Technician{
+    private int experienceYears;
+    public JuniorTech(String name, int age, float salary, int projects, int experience){
+        super(name,age,salary, projects);
+        experienceYears = experience;
+
+    }
+}
