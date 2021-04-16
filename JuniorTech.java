f8df125dde31ab4c268373216e1b20193709a261
bavshehata <bavshehata@gmail.com>
2021-04-16 09:34:18 +0200

    overriding methods

--- JuniorTech.java
+++ JuniorTech.java
@@ -1,8 +1,13 @@
-public class JuniorTech extends Technician{
+public final class JuniorTech extends Technician{
     private int experienceYears;
     public JuniorTech(String name, int age, float salary, int projects, int experience){
         super(name,age,salary, projects);
         experienceYears = experience;
 
+    }
+    // Overriding (1)
+    public void display(){
+        super.display(); // optional
+        System.out.println("And the junior dude");
     }
 }
--- Technician.java
+++ Technician.java
@@ -1,8 +1,15 @@
+
 public class Technician extends Employee {
+
     private int projects_done;
     public Technician(String name, int age, float salary, int projects){
         super(name,age,salary);
         projects_done = projects;
 
     }
+    // Overriding (1)
+    public void display(){
+        System.out.println("This is the tech");
+    }
+
 }
