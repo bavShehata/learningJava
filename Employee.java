756510ee9970d3aef0901bbad034e48f69f18439
bavshehata <bavshehata@gmail.com>
2021-04-20 12:51:57 +0200

    protected methods

--- Employee.java
+++ Employee.java
@@ -1,37 +1,43 @@
-// Packages (3)
 import Faculties.ICS;
 
 public class Employee extends ICS {
     private String name;
-// Packages (3)
-ICS c1 = new ICS();
-Employee c2 = new Employee();
 
-private String deptName = dName;
-private int age;
-private double salary;
-private static int count=0;
-private final static int hours = 20;
-public Employee(){
+    private String deptName = dName;
+    private int age;
+    private double salary;
+    private static int count = 0;
+    private final static int hours = 20;
 
-}
-public Employee(String name, int age, double salary){
-    this.name = name;
-    this.age = age;
-    this.salary = salary;
-    count++;
-}
-public static int getCount(){
-    return count;
-}
-public void setAge(int x){
-    age = x;
-}
-public String toString(){
-    return "Employer: " + name + " is " + age + " years old";
-}
-    private static void main() {
+    public Employee() {
+
+    }
+
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public void setAge(int x) {
+        age = x;
+    }
+
+    public String toString() {
+        return "Employer: " + name + " is " + age + " years old";
+    }
+
+    public static void main() {
+        ICS c1 = new ICS();
+        Employee c2 = new Employee();
+        // protected (1)
+        c1.test(); // error
+        c2.test(); // works
         
     }
-    
 }
--- Faculties/ICS.java
+++ Faculties/ICS.java
@@ -2,4 +2,8 @@ package Faculties;
 
 public class ICS {
     public String dName="ICS";
+    // protected (1)
+    protected void test(){
+        System.out.println("TEST");
+    }
 }
