9272649b382ca06ca94fa64608d95422f3614d5f
bavshehata <bavshehata@gmail.com>
2021-04-23 23:52:35 +0200

    Final class

---
+++ BabyTech.java
@@ -0,0 +1,8 @@
+// Final class (2)
+public class BabyTech extends JuniorTech{
+    public int milk;
+    public BabyTech(String name, int age, float salary, int projects, int experience, int m){
+        super(name,age,salary, projects, experience);
+        milk = m;
+    }
+}
--- JuniorTech.java
+++ JuniorTech.java
@@ -1,18 +1,17 @@
-public final class JuniorTech extends Technician{
+// Final class (2)
+public class JuniorTech extends Technician{
     private int experienceYears;
     public JuniorTech(String name, int age, float salary, int projects, int experience){
         super(name,age,salary, projects);
         experienceYears = experience;
-
     }
-    // Overriding (1)
     public void display(){
         super.display(); // optional
         System.out.println("And the junior dude");
     }
-
-    // Final (2)
-    public final void show(){
-        System.out.println("This won't work");
-    }
+    // Abstract method(4)
+    public abstract void test();
+    // public final void show(){
+    //     System.out.println("This won't work");
+    // }
 }
